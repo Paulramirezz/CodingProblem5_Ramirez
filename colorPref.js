75dcc7789900a9349b1286906eb9a7a2b825c7e8
Paulramirezz <ramirezjohnpaul747@gmail.com>
2024-11-04 22:05:14 +0800

    Create colorPref.js

---
+++ colorPref.js
@@ -0,0 +1,24 @@
+function colors)
+｛
+2
+let favcolors - [];
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+}
+for (let i = 0; i < 3; i++) {
+let enterColor - prompt"Enter Your Three Favorite colors.
+"I;
+facolors-push(enterColor);
+alert("Your Favorite Colors are:" + favcolors);
+let pushcolor - prompt("Add another color you like."); favcolors -push(pushcolor);
+alert("Your new colors are:" + favcolors)
+
+}
